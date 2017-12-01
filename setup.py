f9a9ddd9a619357036ff00b3410b40efe771a242
jbarbadillo <javier.barbadillo@gmail.com>
2017-12-01 22:55:45 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name = 'pyFSM',
+    version='1.0',
+    packages = ['state_machine'],
+    license = 'AGPL v3',
+    description = 'FSM for event driven applications',
+    url = 'https://github.com/jbarbadillo/pyFSM',
+    keywords = ['fsm', 'state-machine'],
+    classifiers = ['Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+)
