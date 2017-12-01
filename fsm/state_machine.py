4c8f5bf9d5a16bc5eb4a9689bc5bd2534c5ba372
jbarbadillo <javier.barbadillo@gmail.com>
2017-12-02 00:39:33 +0100

    Add startFSM implementation

--- fsm/state_machine.py
+++ fsm/state_machine.py
@@ -7,14 +7,16 @@ class State:
 
 class StateMachine:
     currentState = None
+    initialState = None
 
     def __init__(self, states, initial_state, events, transitions):
         self._states = []
         self.addStates(states)  # possible states
         self._events = events  # allowed events
         self.initialState = initial_state
-        self.currentState = self.getStateByName(initial_state)
+
         self.addTransitions(transitions)
+        self.started = False
         print("FSM created!")
 
     def propagateEvent(self, event):
@@ -73,6 +75,15 @@ class StateMachine:
 
     def getCurrentState(self):
         return self.currentState
-    # def startFSM(self):
+
+    def startFSM(self):
+        if (self.initialState != None and self.currentState == None):
+            self.currentState = self.getStateByName(self.initialState)
+            self.started = True
+            # TODO execute run on current state
+            print("StateMachine::Started!")
+            return True
+        else:
+            return False
     # def stopFSM(self):
     # def updateFSM(self):
--- tests/testFSM.py
+++ tests/testFSM.py
@@ -13,10 +13,24 @@ class TestCreateFSM(unittest.TestCase):
         transitions.append(["RUNNING", "finish", "IDLE"])
         self.state_machine = fsm.StateMachine(states,initial_state, events, transitions)
 
-        self.assertEqual(self.state_machine.getCurrentState().name, "IDLE")
+        self.assertEqual(self.state_machine.initialState, "IDLE")
         self.assertEqual(len(self.state_machine._states), 3)
         self.assertEqual(len(self.state_machine._events), 3)
+        self.assertEqual(self.state_machine.currentState, None)
 
+    def test_start_FSM(self):
+        states = ["READY", "RUNNING", "IDLE"]
+        initial_state = "IDLE"
+        events = ["initialized", "start", "finish"]
+        transitions = []
+        transitions.append(["IDLE", "initialized", "READY"])
+        transitions.append(["READY", "start", "RUNNING"])
+        transitions.append(["RUNNING", "finish", "IDLE"])
+        self.state_machine = fsm.StateMachine(states, initial_state, events, transitions)
 
+        self.assertEqual(self.state_machine.started, False)
+        self.state_machine.startFSM()
+        self.assertEqual(self.state_machine.currentState.name, "IDLE")
+        self.assertEqual(self.state_machine.started, True)
 if __name__ == '__main__':
     unittest.main()
