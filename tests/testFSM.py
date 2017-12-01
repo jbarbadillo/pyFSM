6b5717c94d9b09f24ad12849c1f95332a34978d3
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-12-02 00:44:26 +0100

    Minor format fixes

--- tests/testFSM.py
+++ tests/testFSM.py
@@ -6,12 +6,12 @@ class TestCreateFSM(unittest.TestCase):
     def test_create_state_machine(self):
         states = ["READY", "RUNNING", "IDLE"]
         initial_state = "IDLE"
-        events = ["initialized","start","finish"]
-        transitions=[]
-        transitions.append(["IDLE","initialized","READY"])
+        events = ["initialized", "start", "finish"]
+        transitions = []
+        transitions.append(["IDLE", "initialized", "READY"])
         transitions.append(["READY", "start", "RUNNING"])
         transitions.append(["RUNNING", "finish", "IDLE"])
-        self.state_machine = fsm.StateMachine(states,initial_state, events, transitions)
+        self.state_machine = fsm.StateMachine(states, initial_state, events, transitions)
 
         self.assertEqual(self.state_machine.getCurrentState().name, "IDLE")
         self.assertEqual(len(self.state_machine._states), 3)
