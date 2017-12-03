5680573f2131dd9b6b84712e3b8d844cdc298544
jbarbadillo <javier.barbadillo@gmail.com>
2017-12-03 20:17:14 +0100

    Update error message

--- pyfsm/state_machine.py
+++ pyfsm/state_machine.py
@@ -98,7 +98,7 @@ class StateMachine:
         # checks that all states have valid transitions and a function bound
         for state in self._states:
             if len(state._transitions) < 1 or self.isEmptyFunction(state.run):
-                print("Empty function! You must assing a function to state")
+                print("State %s cannot have an empty function! Asign a funtion and start again" % state.name)
                 return False
 
         return True
