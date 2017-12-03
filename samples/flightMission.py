002882016b79db5d82c52d17f0209d458f4c5ad6
jbarbadillo <javier.barbadillo@gmail.com>
2017-12-03 20:23:04 +0100

    Update sample

--- samples/flightMission.py
+++ samples/flightMission.py
@@ -71,7 +71,26 @@ def runFlightMission():
         print("Could not create FSM!")
         return False
 
+    # bind functions
+    state_machine._states[0].run = idle
+    state_machine._states[1].run = ready
+    state_machine._states[2].run = takeOff
+    state_machine._states[3].run = point1
+    state_machine._states[4].run = point2
+    state_machine._states[5].run = point3
+    state_machine._states[6].run = point4
+    state_machine._states[7].run = point5
+    state_machine._states[8].run = land
+    state_machine._states[9].run = abort
+
     state_machine.startFSM()
+
+    state_machine.propagateEvent("initialized")
+
+    state = state_machine.propagateEvent("start_mission")
+
+    while state != "LANDING":
+        state= state_machine.propagateEvent("position_reached")
 
 # Call main application
 runFlightMission()
